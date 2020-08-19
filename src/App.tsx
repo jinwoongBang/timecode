9a6a62a5cfa7165f508f92f83914f31d7af27db8
jinwoong-bang <jinwoong-bang@jinwoong-bangui-MacBookPro.local>
2020-08-19 22:27:24 +0900

    브랜치 생성 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import React, { useEffect } from 'react';
 import CounterContainer from 'containers/CounterContainer';
 
 function App() {
-  return <CounterContainer></CounterContainer>;
+  return <CounterContainer />;
 }
 export default App;
